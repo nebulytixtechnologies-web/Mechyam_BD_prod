85c5cce54f3ef8ee306bc66fd632e71e58c51587
LikithRoshanReddyKankanala <146204708+LikithRoshan.Kankanala@users.noreply.github.com>
2026-01-03 18:32:47 +0530

    ...

--- src/main/java/com/mechyam/controller/ClientController.java
+++ src/main/java/com/mechyam/controller/ClientController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/api/clients")
 @CrossOrigin(origins = "*")
 public class ClientController {
 
